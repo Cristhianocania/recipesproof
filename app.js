f58fd083bbedd7d68befe47eee2d6b678a24e37e
cristhianocania <cris.ocania@gmail.com>
2022-05-27 02:42:59 -0300

    addition jwt1

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var recipesRouter = require('./routes/recipes');
 
 
+
 /*configuracion de mongoose para la conexion*/
 mongoose.Promise= global.Promise
 mongoose.connect(

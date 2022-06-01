33cb4a139759258431dc348249d25573f965aa30
cristhianocania <cris.ocania@gmail.com>
2022-06-01 20:12:21 -0300

    asd

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
 app.use('/', indexRouter);
-app.use('/recipes', recipesRouter);
+app.use('/recipess', recipesRouter);
 
 
 app.listen(process.env.PORT || 5000); //nos da el puerto heroku por defecto en caso de que no va estar el 5000

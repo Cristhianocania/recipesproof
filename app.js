4aaa6d91f99e94feb7c4f3447c7fd38d809cd05e
cristhianocania <cris.ocania@gmail.com>
2022-06-02 20:51:34 -0300

    commitx

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const bodyParser= require('body-parser');
 const cors = require('cors');
 const app = express();//instancia de express
-
+const jwt = require('jsonwebtoken');
 const routes = require ('./routes');
+
+app.set('secretKey',"sebastian")
+
 
 /*configuracion de mongoose para la conexion*/
 mongoose.Promise= global.Promise
@@ -23,21 +26,23 @@ app.use(bodyParser.urlencoded({extended:true}));
 
     //habilitar cors
 app.use(cors());
-
 app.use('/',routes());
 
- // habilitar la carpeta uploads como carpeta statica y publica para que un cliete vea la imagen de un producto
+function validateUser(req,res,next){
+    jwt.verify(req.headers['x-access-token'],req.app.get("secretKey"),function(err,decoded){
+      if(err){
+        res.json({message:err.message})
+      }else{
+        console.log(decoded)
+        req.body.tokenData = decoded;
+        next();
+      }
+    })
+  }
+  app.validateUser = validateUser;
 
 
- 
-
-/* app.get('/', function(req,res){
-
-    res.send('hola mundo ee eeeeeeeeeeeeeeeeeeewey');
-});
-
-/*ruta de tipo get .
-REQUEST= peticion que llega a nuestro servidor web o backend. 
-response=objeto a traves el cual nuestro servidor va a responder*/
 
 app.listen(process.env.PORT || 5000); //nos da el puerto heroku por defecto en caso de que no va estar el 5000
+
+module.exports = app;

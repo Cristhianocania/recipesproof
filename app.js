7afee9a18a13fcc100e511f320cff97adf4a37b7
cristhianocania <cris.ocania@gmail.com>
2022-06-01 21:18:12 -0300

    asd

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ app.use(bodyParser.urlencoded({extended:true}));
     //habilitar cors
 app.use(cors());
 
-app.use('/', indexRouter);
+app.use('/', (req,res)=>
+res.send("HOLA"));
+ 
 app.use('/recipess', recipesRouter);
 
 
